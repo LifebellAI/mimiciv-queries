5c0dadd99d9c89826d7f8e18257217baf8c56028
rjaved111 <78034437+rjaved111@users.noreply.github.com>
2021-04-02 13:28:43 -0400

    Create demographic_and_stay_data.sql

---
+++ demographic_and_stay_data.sql
@@ -0,0 +1,27 @@
+SELECT
+  subject_id,
+  hadm_id,
+  stay_id,
+  admittime,
+  deathtime,
+  admission_type,
+  discharge_location,
+  first_careunit,
+  last_careunit,
+  intime,
+  outtime,
+  los as total_los,
+  ethnicity,
+  gender,
+  anchor_age
+FROM
+  `physionet-data.mimic_core.admissions`
+JOIN
+  `physionet-data.mimic_core.patients`
+USING
+  (subject_id)
+JOIN
+  `physionet-data.mimic_icu.icustays`
+USING
+  (subject_id,
+    hadm_id)
