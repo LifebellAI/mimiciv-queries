51977654d1968e0c3988ff8d2b7d77f6c522cfa6
rjaved111 <78034437+rjaved111@users.noreply.github.com>
2021-04-02 14:42:40 -0400

    Create hourly_abg_pivoted.sql

---
+++ hourly_abg_pivoted.sql
@@ -0,0 +1,12 @@
+SELECT 
+subject_id,
+hadm_id,
+stay_id,
+charttime,
+AVG(if (label ='Arterial O2 pressure', value, null)) as PaO2,
+AVG(if (label ='Arterial O2 Saturation', value, null)) as SaO2,
+AVG(if (label ='Arterial CO2 Pressure', value, null)) as PaCO2,
+AVG(if (label ='PH (Arterial)', value, null)) as pH,
+AVG(if (label ='Arterial Base Excess', value, null)) as base_excess,
+FROM `elevated-pod-307118.physionet.hourly_abg_flat`
+GROUP BY subject_id, hadm_id, stay_id, charttime
