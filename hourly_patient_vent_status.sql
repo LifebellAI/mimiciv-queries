5ffb630c091a4caf6282c5b0afa2ac5056e46f7f
rjaved111 <78034437+rjaved111@users.noreply.github.com>
2021-04-05 10:03:26 -0400

    Create hourly_patient_vent_status.sql

---
+++ hourly_patient_vent_status.sql
@@ -0,0 +1,24 @@
+-- This query uses the table "procedureevents" which records when a patient was ventilated and then when they were unventilated
+-- In order to expand this into the hourly format we need, I've used the "Generate Timestamp Array" function provided by bigquery: 
+-- See: https://cloud.google.com/bigquery/docs/reference/standard-sql/array_functions#generate_date_array
+
+SELECT
+  subject_id,
+  hadm_id,
+  stay_id,
+  DATETIME_TRUNC(starttime,
+    HOUR) AS charttime,
+  starttime,
+  endtime,
+  day,
+  TRUE AS ventilated
+FROM
+  `physionet-data.mimic_icu.procedureevents`
+JOIN
+  UNNEST(GENERATE_TIMESTAMP_ARRAY(TIMESTAMP(DATETIME_TRUNC(starttime,
+          HOUR)),
+      TIMESTAMP(DATETIME_TRUNC(endtime,
+          HOUR)),
+      INTERVAL 1 HOUR)) DAY
+WHERE
+  ordercategoryname = "Ventilation"
