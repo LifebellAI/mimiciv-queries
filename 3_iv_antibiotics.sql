0335bb552925f348b575e98d65cb67528f13cef7
rjaved111 <78034437+rjaved111@users.noreply.github.com>
2021-04-05 10:45:08 -0400

    Update 3_iv_antibiotics.sql

--- 3_iv_antibiotics.sql
+++ 3_iv_antibiotics.sql
@@ -1,6 +1,8 @@
 -- This query collects all of the rows recording iv antibiotics, from the larger table of all ICU inputevents
 -- We include both rate and amount because data from one EMR comes with a calculated rate (ml/hr), 
 -- whereas data from the other EMR comes with only an amount and a total amount (rate presumably can be calculated from this)
+CREATE OR REPLACE TABLE `physionet.iv_antibiotics` as
+
 SELECT
 subject_id,
 hadm_id,
