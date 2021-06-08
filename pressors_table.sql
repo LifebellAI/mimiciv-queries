9b8d3517e3fc7e385c4cd328d4d9a9d3fb9fb086
edward-data-sci <edward@ricealumni.net>
2021-06-08 07:13:02 +0000

    modifications to pressors table filter and more comments

--- pressors_table.sql
+++ pressors_table.sql
@@ -10,10 +10,13 @@ DATETIME_TRUNC(`physionet-data.mimic_hosp.prescriptions`.starttime, HOUR) as cha
 drug, 
 medication, 
 prod_strength, 
-if(dose_unit_rx='mg', cast(dose_val_rx as numeric)*1000, cast(dose_val_rx as numeric)) as dose_val_rx, 
-'mg' as dose_unit_rx, 
+if(dose_unit_rx='mg', cast(dose_val_rx as numeric)*1000, cast(dose_val_rx as numeric)) as dose_val_rx, --labeler expects pressors in mcg/kg/min
+'mcg' as dose_unit_rx, 
 duration, 
 duration_interval
+-- note: whenever joining between prescriptions and pharmacy, make sure your medication field / drug field filters are applied to both tables (especially drug)
+-- since pharmacy orders can comprise of multiple prescriptions, you may end up with multiple records in the final table 
+-- with each record representing one of potentially multiple drugs being used to fulfill a pharmacy order (which will invalidate dosage aggregations)
 from `physionet-data.mimic_hosp.prescriptions`
 join `physionet-data.mimic_hosp.pharmacy`
 using(subject_id, hadm_id, pharmacy_id)
@@ -21,13 +24,21 @@ join `physionet-data.mimic_icu.icustays`
 using (subject_id, hadm_id)
 where 
 (
-    lower(drug) like '%epinephrine%'
-     or lower(drug) like '%dopamine%'
-     or lower(drug) like '%dobutamine%'
+    lower(`physionet-data.mimic_hosp.prescriptions`.drug) like '%epinephrine%'
+     or lower(`physionet-data.mimic_hosp.prescriptions`.drug) like '%dopamine%'
+     or lower(`physionet-data.mimic_hosp.prescriptions`.drug) like '%dobutamine%'
 )
 and
+(
+    lower(`physionet-data.mimic_hosp.pharmacy`.medication) like '%epinephrine%'
+     or lower(`physionet-data.mimic_hosp.pharmacy`.medication) like '%dopamine%'
+     or lower(`physionet-data.mimic_hosp.pharmacy`.medication) like '%dobutamine%'
+)
+
+and
+-- the below filters remove a very small % of records that are problematic
 lower(`physionet-data.mimic_hosp.prescriptions`.route) in ('iv', 'iv drip')
 and
-dose_val_rx not like "%-%"
+dose_val_rx not like "%-%" --todo: determine how to transform these (take highest number from range?)
 and
 dose_unit_rx in ('mg', 'mcg');
