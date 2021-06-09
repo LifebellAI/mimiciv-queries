21a726e299a0119222876778b26ce5051da0defe
edward-data-sci <edward@ricealumni.net>
2021-06-09 05:13:09 +0000

    inclusion of patient weight in demographic and stay data table

--- 1_demographic_and_stay_data.sql
+++ 1_demographic_and_stay_data.sql
@@ -16,7 +16,9 @@ SELECT
   los as total_los,
   ethnicity,
   gender,
-  anchor_age
+  anchor_age,
+  weight,
+  weight_units
 FROM
   `physionet-data.mimic_core.admissions`
 JOIN
@@ -28,6 +30,18 @@ JOIN
 USING
   (subject_id,
     hadm_id)
+LEFT JOIN 
+(
+    select subject_id, round(avg(valuenum), 2) as weight, max(valueuom) as weight_units 
+    from  `physionet-data.mimic_icu.chartevents`
+    where valuenum is not null
+    -- admission weight itemid: 226512
+    -- note: may want to also consider itemid 224639 (daily weight) in the future
+    and itemid = 226512 
+    and valuenum > 0
+    group by subject_id
+)
+USING (subject_id)
 JOIN
   UNNEST(GENERATE_TIMESTAMP_ARRAY(TIMESTAMP(DATETIME_TRUNC(admittime,
           HOUR)),
